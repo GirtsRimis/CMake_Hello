4350e4e44a82c3697bbd59bf5194b427c5f8d312
Girts <girts60@gmail.com>
2020-11-22 17:09:54 +0200

    1.0

---
+++ hello_world.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "hello_world.h"
+
+void hello::print_hello()
+{
+    std::cout << "Hello World!";
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "hello_world.h"
+
+int main()
+{
+    hello::print_hello();
+    return 0;
+}
